c405fb3d8624db5db526c26fd0e549ab8e509436
bruno_mocho <170221059@estudantes.ips.pt>
2024-02-07 11:55:45 +0000

    Refresh of views to display real data from database

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ClubController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _dbContext.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RaceController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _dbContext.Races.ToList();
+            return View(races);
         }
     }
 }
